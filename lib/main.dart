5fa422093f7388e385b688d246ad8d1fc97fcd4c
Baek Young Hoon <yyh0965@gmail.com>
2023-09-15 21:34:42 +0900

    리스트뷰 추가
    데이터 넣어야함

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -26,52 +28,67 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int num = 1;
+  int num = 0;
+  List<String> items = List.generate(100, (index) => "물건 $index");
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-              "ActionStore",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,  // 글꼴 두께 (볼드)
-                fontStyle: FontStyle.italic,  // 글꼴 스타일 (이탤릭)
-              )
-          )
+        title: const Text(
+          "ActionStore",
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children : [
-          TextButton(
-            onPressed: (){
+      body: ListView.builder(
+        itemCount: num, // 항목의 수
+        itemBuilder: (BuildContext context, int index) {
+          return InkWell(
+            onTap: () {
               setState(() {
-                num = 1;
+                if(items[index] == "물건 클릭") items[index] = "물건 $index";
+                else items[index] = "물건 클릭";
               });
             },
-            child: Text("$num"),
-          )
-        ],
+            child: Container(
+              height: 80,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
+                  width: 0.7,
+                ),
+              ),
+              child: ListTile(
+                titleAlignment: ListTileTitleAlignment.center,
+                title: Text(items[index]),
+              ),
+            ),
+          );
+        },
       ),
       bottomNavigationBar: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           IconButton(
-              onPressed: (){
-                setState((){
-                  num++;
-                });
-              },
-              icon: Icon(Icons.card_travel)),
-          IconButton(onPressed:(){
-            setState(() {
-              num = num + num;
-            });
-          },
-              icon: Icon(Icons.account_circle)
-          )
+            onPressed: () {
+              setState(() {
+                num++;
+              });
+            },
+            icon: Icon(Icons.card_travel),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                num = num + num;
+              });
+            },
+            icon: Icon(Icons.account_circle),
+          ),
         ],
       ),
     );
