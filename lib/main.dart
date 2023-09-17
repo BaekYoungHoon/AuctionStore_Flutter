e55afae40c952c7fd31781440586e08d17eea4b1
Baek Young Hoon <yyh0965@gmail.com>
2023-09-17 20:44:07 +0900

    메인피드와 임시 상품등록 화면전환

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
+int Screen = 0;
+int num = 0;
+List<String> items = List.generate(100, (index) => "물건 $index");
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -28,8 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int num = 0;
-  List<String> items = List.generate(100, (index) => "물건 $index");
+
 
   @override
   Widget build(BuildContext context) {
@@ -44,32 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: ListView.builder(
-        itemCount: num, // 항목의 수
-        itemBuilder: (BuildContext context, int index) {
-          return InkWell(
-            onTap: () {
-              setState(() {
-                if(items[index] == "물건 클릭") items[index] = "물건 $index";
-                else items[index] = "물건 클릭";
-              });
-            },
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 0.7,
-                ),
-              ),
-              child: ListTile(
-                titleAlignment: ListTileTitleAlignment.center,
-                title: Text(items[index]),
-              ),
-            ),
-          );
-        },
-      ),
+      body: BodyView(),
       bottomNavigationBar: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
@@ -77,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () {
               setState(() {
                 num++;
+                Screen = 0;
               });
             },
             icon: Icon(Icons.card_travel),
@@ -85,12 +63,59 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () {
               setState(() {
                 num = num + num;
+                Screen = 1;
               });
             },
             icon: Icon(Icons.account_circle),
           ),
         ],
       ),
+    );
+  }
+  Widget BodyView(){
+    if(Screen == 0){
+      return ListView.builder(
+            itemCount: num, // 항목의 수
+            itemBuilder: (BuildContext context, int index) {
+              return InkWell(
+                onTap: () {
+                  setState(() {
+                    if(items[index] == "물건 클릭") items[index] = "물건 $index";
+                    else items[index] = "물건 클릭";
+                  });
+                },
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 0.7,
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: ListTile(
+                          titleAlignment: ListTileTitleAlignment.center,
+                          title: Text(items[index]),
+                        ),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("물건"),
+                          Text("가격")
+                        ],
+                      )
+                    ],
+                  ),
+
+                ),
+              );
+            },
+          );
+    }else return Scaffold(
+      body: Text("아이템 등록 할거임"),
     );
   }
 }
